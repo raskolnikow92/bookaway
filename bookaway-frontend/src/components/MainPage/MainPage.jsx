090ab48cc63553cad21562fa38cb8dd2e087d24f
Kai Schuleit <kai.schuleit@hotmail.de>
2025-06-03 15:04:14 +0200

    change from js to jsx

---
+++ bookaway-frontend/src/components/MainPage/MainPage.jsx
@@ -0,0 +1,14 @@
+export function MainPage(){
+
+    return (
+        <div className="viewContainer">
+            <div className="textContainer">
+            </div>
+            <div className="booksContainer">
+                <div className="bookCard">
+                    book
+                </div>
+            </div>
+        </div>
+    )
+}
