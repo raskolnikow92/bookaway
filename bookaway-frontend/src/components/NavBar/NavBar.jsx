26f9ea1be7614a1223a96c3d4c1701bf53820a75
kaisc <kai.schuleit@hotmail.de>
2025-06-03 11:08:55 +0200

    implement navbar

--- bookaway-frontend/src/components/NavBar/NavBar.jsx
+++ bookaway-frontend/src/components/NavBar/NavBar.jsx
@@ -1,29 +1,32 @@
-function NavBar(){
-    
-    return (
-        <div>
-            <nav className="navbar navbar-expand-lg navbar-light bg-light" id='navbar_container'>
-                <a className="navbar-brand" href="#">Bookaway</a>
-                <div className="justify-content-end">
-                    <ul className="navbar-nav">
-                        <li className="nav-item">
-                            <a className="nav-link">Home</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link">Genres</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link">Search</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link">Login</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link">Register</a>
-                        </li>
-                    </ul>
-                </div>
-            </nav>
-        </div>
-    )
+import React from 'react';
+
+function NavBar() {
+  return (
+    <nav className="navbar navbar-expand-lg bg-body-tertiary fixed-top">
+  <div className="container-fluid">
+    <a className="navbar-brand" href="#">Navbar</a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarNav">
+      <ul className="navbar-nav">
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Features</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Pricing</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link disabled" aria-disabled="true">Disabled</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+  );
 }
+
+export default NavBar;
