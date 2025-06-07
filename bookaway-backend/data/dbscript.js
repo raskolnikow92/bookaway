2f1dccffe6e88604412160a362b032dbf695b69e
Kai Schuleit <kai.schuleit@hotmail.de>
2025-06-07 19:55:22 +0200

    db script

--- bookaway-backend/data/dbscript.js
+++ bookaway-backend/data/dbscript.js
@@ -1,3 +1,22 @@
+require('dotenv').config();
 const { connectToDB } = require("../db/Connection");
+const fs = require('fs');
+const path = require('path');
 
-connectToDB
+
+async function fillDatabase(){
+
+    console.log('in der function fillDatabase');
+    try{
+        const db = await connectToDB();
+        const collection = db.collection("books");
+        const filePath = path.join(__dirname, 'books_for_mongodb.json');
+        const booksData = JSON.parse(fs.readFileSync(filePath, 'utf-8'));
+        const result = await collection.insertMany(booksData);
+        console.log (`Es wurden ${result.insertedCount} Bücher importiert!`)
+    }catch(err){
+        console.log(err);
+    }
+}
+
+fillDatabase();
